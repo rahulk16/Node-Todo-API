a7f608367d566192b8cfd9670e979edb431843c6
RAHUL KUMAR <rahul162rk@gmail.com>
2019-02-08 18:35:07 +0530

    Update mongodb-find.js

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,20 +8,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,client) => {
   console.log('Connected to server.');
   const db = client.db();
 
-  // db.collection('TodoApp').find({
-  //   _id:new ObjectID('5c5600d88ea25e88804e4f15')
-  // }).toArray().then((docs) => {
-  //   console.log('TodoApp');
-  //   console.log(JSON.stringify(docs,undefined,2));
-  // },(err) => {
-  //   console.log('Unable to fetch error',err);
-  // });
+  db.collection('TodoApp').find({
+    _id:new ObjectID('5c5600d88ea25e88804e4f15')
+  }).toArray().then((docs) => {
+    console.log('TodoApp');
+    console.log(JSON.stringify(docs,undefined,2));
+  },(err) => {
+    console.log('Unable to fetch error',err);
+  });
 
-  // db.collection('TodoApp').find().count().then((count) => {
-  //   console.log('TodoApp Count: ',count);
-  // },(err) => {
-  //   console.log('Unable to fetch error',err);
-  // });
+  db.collection('TodoApp').find().count().then((count) => {
+    console.log('TodoApp Count: ',count);
+  },(err) => {
+    console.log('Unable to fetch error',err);
+  });
 
   db.collection('User').find({name:'Thunder'}).count().then((count) => {
     console.log('User Thunder Count: ',count);
